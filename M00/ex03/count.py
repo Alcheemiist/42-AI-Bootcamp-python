ce33475c72d4d91b65be4a5a756fe9656da4c8ae
Elmahdi ELaazmi <elaazmielmahdi@gmail.com>
2021-04-02 04:18:58 +0100

    handle ex03 errors

--- M00/ex03/count.py
+++ M00/ex03/count.py
@@ -1,6 +1,15 @@
-def text_analyzer(text):
-    lent = len(text) 
-    print ("this text fcontain " ,lent, "characters :")
+def text_analyzer(*text):
+    """
+      ------------------------------------------------------------
+    | This function counts the number of upper characters,        |
+    | lower characters, punctuation and spaces in a given text.   |
+      ------------------------------------------------------------
+    """
+    if (len(text) > 1 or len(text) == 0):
+        print("ERROR")
+        exit()
+    lent = len(text[0]) 
+    print ("this text contain " ,lent, "characters :")
     p = l = u = s = 0  
     for i in text:
         if str(i) == ('.') or str(i) == (',') or str(i) == ('?') or str(i) == ('!') or str(i) == (',') or str(i) == ("'") or str(i) == ("-") :
@@ -11,12 +20,8 @@ def text_analyzer(text):
             l += 1
         elif str(i).isspace():
             s += 1
-
     print ("- pon = ", p)
     print ("- UPPER = ", u)
     print ("- lower = ", l)
     print ("- space = ", s)
-
     print(" p + u + l + s =", u + p + l + s)
-
-text_analyzer("Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.")

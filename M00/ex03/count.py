6ba4abb822cfe7fbccc9aa75e792ec2a04d9a7d6
Elmahdi ELaazmi <elaazmielmahdi@gmail.com>
2021-04-04 05:04:28 +0100

    add input when no str passed into func

--- M00/ex03/count.py
+++ M00/ex03/count.py
@@ -1,17 +1,33 @@
+import sys 
+
+def get_input():
+    txt = ""
+    print("text to analyze")
+    for line in sys.stdin:
+        txt += line
+    return txt
+
+
 def text_analyzer(*text):
+
     """
       ------------------------------------------------------------
     | This function counts the number of upper characters,        |
     | lower characters, punctuation and spaces in a given text.   |
       ------------------------------------------------------------
     """
-    if (len(text) > 1 or len(text) == 0):
+    if (len(text) > 1):
         print("ERROR")
         exit()
-    lent = len(text[0]) 
+    elif len(text) == 0:
+        txt = get_input()
+    else :
+        txt = text[0]
+
+    lent = len(txt) 
     print ("this text contain " ,lent, "characters :")
     p = l = u = s = 0  
-    for i in text:
+    for i in txt:
         if str(i) == ('.') or str(i) == (',') or str(i) == ('?') or str(i) == ('!') or str(i) == (',') or str(i) == ("'") or str(i) == ("-") :
            p += 1 
         elif str(i).isupper():
@@ -25,3 +41,6 @@ def text_analyzer(*text):
     print ("- lower = ", l)
     print ("- space = ", s)
     print(" p + u + l + s =", u + p + l + s)
+
+
+text_analyzer()
